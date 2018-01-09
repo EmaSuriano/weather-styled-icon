a5a1e13838978e95241c86f964fed4dd01cb2013
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2018-01-08 23:34:32 -0500

    adding test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 export default class extends Component {
   render() {
     return <div>
-      <h2>Welcome to React components</h2>
+      <h2>Welcome to React components2</h2>
     </div>
   }
 }
