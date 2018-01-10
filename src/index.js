117bc400eb9ba760a7aaf50d6658234edfab2dfa
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2018-01-09 22:16:38 -0500

    adding a branch to test coverall

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react'
 
 export default class extends Component {
+  state = {
+    a: 1,
+  }
+
   render() {
     return <div>
       <h2>Welcome to the Wonderful World of npm</h2>
+      { a ? <p>asdasd</p > :< p>qweqwe</p > }
     </div>
   }
 }
