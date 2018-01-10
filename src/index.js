0b7d932010a5f5923c6e5d2443a76031ad8ce079
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2018-01-09 20:27:04 -0500

    now for sure breaking the test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 export default class extends Component {
   render() {
     return <div>
-      <h2>Welcome to React components 2</h2>
+      <h2>Welcome to the Wonderful World of npm</h2>
     </div>
   }
 }
