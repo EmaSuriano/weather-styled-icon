3fc26a229364187cec0f00a0b419bfb118ae267a
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2018-01-09 22:18:22 -0500

    prettier rocks:

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 export default class extends Component {
   state = {
-    a: 1,
-  }
+    a: 1
+  };
 
   render() {
-    return <div>
-      <h2>Welcome to the Wonderful World of npm</h2>
-      { a ? <p>asdasd</p > :< p>qweqwe</p > }
-    </div>
+    const a = 1;
+    return (
+      <div>
+        <h2>Welcome to the Wonderful World of npm</h2>
+        {a ? <p>asdasd</p> : <p>qweqwe</p>}
+      </div>
+    );
   }
 }
