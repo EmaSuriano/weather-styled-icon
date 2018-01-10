6d7dc880499903d7b8f842d9cdb2871cc0e35304
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2018-01-09 22:36:33 -0500

    adding enzyme

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,23 +1,20 @@
 import expect from "expect";
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
+configure({ adapter: new Adapter() });
 import Component from "src/";
 
 describe("Component", () => {
   let node;
 
   beforeEach(() => {
-    node = document.createElement("div");
-  });
-
-  afterEach(() => {
-    unmountComponentAtNode(node);
+    node = shallow(<Component />);
   });
 
   it("displays a welcome message", () => {
-    render(<Component />, node, () => {
-      expect(node.innerHTML).toContain("Welcome to the Wonderful World of npm");
-    });
+    const message = node.find("h2");
+    expect(message.text()).toBe("Welcome to the Wonderful World of npm");
   });
 });
